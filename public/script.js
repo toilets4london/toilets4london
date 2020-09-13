def38a5777a29f9adcc702b13a816fb0f8c0cfdd
Nina Rimsky <ninarimsky@gmail.com>
2020-09-14 00:09:22 +0100

    adding pages

--- public/script.js
+++ public/script.js
@@ -1,9 +1,33 @@
-window.onload = function() {
-  const message = document.getElementById("message")
-  document.getElementById("button").addEventListener("click", () => {
-    console.log("hello", message)
-    fetch("/.netlify/functions/hello-world?name=" + "Netlify")
-      .then((data) => data.json())
-      .then(({ msg }) => console.log(msg) || (message.innerHTML = msg))
+fetch("./header.html")
+  .then(response => {
+    return response.text()
   })
-}
+  .then(data => {
+    document.querySelector("header").innerHTML = data;
+    let navToggle = document.querySelector('.nav-toggle');
+    let nav = document.querySelector('nav');
+
+    function doToggle(e) {
+      e.preventDefault();
+      navToggle.classList.toggle('expanded');
+      nav.classList.toggle('expanded');
+    }
+
+    navToggle.addEventListener('click', function (e) { doToggle(e); });
+
+  });
+
+fetch("./footer.html")
+  .then(response => {
+    return response.text()
+  })
+  .then(data => {
+    document.querySelector("footer").innerHTML = data;
+  });
+
+
+
+
+
+
+
